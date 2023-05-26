474bd9930b495eed567cc0e92e5520889cbf5a82
fs-public <134224417+fs-public@users.noreply.github.com>
2023-05-26 22:18:58 +0200

    Test failing action

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 export const TAX_BRACKET = 0.15
 
-const HARVEST_COMM = 10 // 10 USD or EUR
+let HARVEST_COMM = 10 // 10 USD or EUR
 const HARVEST_COMM_BASE = 500 // ^ for every 500 shares
 const HARVEST_SLIPPAGE = 0.001 // 0.1%
 export const getHarvestLoss = (quantity: number, value: number) => {
