7f77c4f127810c3bcd3cfa8c56857cb3b6f2c1f4
fs-public <134224417+fs-public@users.noreply.github.com>
2023-05-28 15:33:20 +0200

    Rename validator.ts

--- src/process/loader.ts
+++ src/process/loader.ts
@@ -4,7 +4,7 @@ import { CSV_SOURCES, loadAndValidateConfig } from "../config/configLoader"
 import loadData from "./1-loadData"
 import parseOrders from "./2-parseOrders"
 import matchFills from "./3-matchFills"
-import { validatorRecords, validatorOrders, validatorFills } from "./4-validator"
+import { validatorRecords, validatorOrders, validatorFills } from "./validator"
 
 export const performFullReload = async (firstLoad = false) => {
     if (!firstLoad) env.flushErrors()
